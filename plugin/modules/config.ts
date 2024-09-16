9e95d600c5ee8d695c0f235531984eb0fd772a74
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2024-09-16 13:40:28 +1000

    Added config module
    - Added config module to configure plugin

---
+++ plugin/modules/config.ts
@@ -0,0 +1,28 @@
+import path from "node:path";
+
+import type { PluginOption, UserConfig } from "vite";
+import type { FrameworkOptions } from "../types";
+
+export function config(options: FrameworkOptions): PluginOption {
+  return {
+    name: "framework",
+    config(config: UserConfig): UserConfig {
+      return {
+        publicDir: false,
+        build: {
+          outDir: config.build?.outDir ?? path.join(process.cwd(), "assets"),
+          assetsDir: config.build?.assetsDir ?? "",
+          rollupOptions: {
+            input: glob.sync([
+              path.join(
+                process.cwd(),
+                options.modulesDirectory ?? "src",
+                "**/*",
+              ),
+            ]),
+          },
+        },
+      };
+    },
+  };
+}
