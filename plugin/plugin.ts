4de8e4e1fc4c37869e98d882b7c08faeec957ac5
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2024-09-19 13:25:32 +1000

    Updated plugin
    - Updated the main entry point for plugin
    - Added defaults
    - Added config
    - Added inject

--- plugin/plugin.ts
+++ plugin/plugin.ts
@@ -1,5 +1,33 @@
-import type { PluginOption } from "vite";
+import path from "node:path";
 
-export default function Framework(): PluginOption[] {
-  return [];
+import { config } from "./modules/config";
+import { inject } from "./modules/inject";
+
+import type { PluginOption } from "vite";
+import type { FrameworkOptions } from "./types";
+
+export default function framework(options?: FrameworkOptions): PluginOption {
+  const defaults: Required<FrameworkOptions> = {
+    directory: path.join(process.cwd(), options?.directory ?? ""),
+    client: path.join(
+      process.cwd(),
+      "snippets",
+      (options?.client ?? "vite-client").split(".")[0] +
+        "." +
+        (path.extname(options?.client ?? "vite-client.liquid") !== "liquid"
+          ? "liquid"
+          : "liquid"),
+    ),
+    snippet: path.join(
+      process.cwd(),
+      "snippets",
+      (options?.snippet ?? "framework").split(".")[0] +
+        "." +
+        (path.extname(options?.snippet ?? "famework.liquid") !== "liquid"
+          ? "liquid"
+          : "liquid"),
+    ),
+  };
+
+  return [config(defaults), inject(defaults)];
 }
