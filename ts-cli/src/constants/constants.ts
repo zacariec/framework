68f0e3c358c0d9755ee6cf5c63d48102f8b37911
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2024-10-21 10:47:11 +1100

    Created constants
    - Added export for token delimiters

---
+++ ts-cli/src/constants/constants.ts
@@ -0,0 +1,17 @@
+export const ViteServerURL = 'http://localhost:3000';
+
+export const LIQUID_DELIMITERS = {
+    OBJECT_OPEN: '{{',
+    OBJECT_CLOSE: '}}',
+    TAG_OPEN: '{%',
+    TAG_CLOSE: '%}',
+};
+
+export const CONFIG_FILE_NAMES = [
+    'framework.config.js',
+    'framework.config.cjs',
+    'framework.config.mjs',
+    'framework.config.ts',
+    'framework.config.cts',
+    'framework.config.mts',
+];
