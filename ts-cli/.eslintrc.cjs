a847ab17dc9dcd7ec7d036f4cb1eab7e7d7bb83c
zacariec <10770942+zacariec@users.noreply.github.com>
2024-11-15 11:13:02 +1100

    Updated eslint rules

--- ts-cli/.eslintrc.cjs
+++ ts-cli/.eslintrc.cjs
@@ -1,33 +1,32 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'import'],
-    extends: [
-        'airbnb-base',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-    ],
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
     },
-    rules: {
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                js: 'never',
-                jsx: 'never',
-                ts: 'never',
-                tsx: 'never',
-            },
-        ],
-        'no-console': ['error', { allow: ['warn', 'error'] }],
-        'import/prefer-default-export': 'off',
-        'import/order': 'off',
-        
-    }
-}
+  },
+  rules: {
+    'no-restricted-syntax': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'import/prefer-default-export': 'off',
+    'import/order': 'off',
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+    'no-nested-ternary': 'off',
+  },
+};
