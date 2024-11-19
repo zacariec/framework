2b6105dc957279e51baf39c9a2b8c0c5df921672
zacariec <10770942+zacariec@users.noreply.github.com>
2024-11-20 10:04:11 +1100

    Updated setup file to include eslint ignore rules

--- ts-cli/src/core/setup.ts
+++ ts-cli/src/core/setup.ts
@@ -11,6 +11,7 @@ export async function initialSetup(): Promise<void> {
   const layoutFiles = await fs.readdir(layoutDirectory);
   const filePromises = layoutFiles.map(
     (file) =>
+      // eslint-disable-next-line no-async-promise-executor
       new Promise(async (resolve) => {
         const filePath = path.join(layoutDirectory, file);
         const content = await fs.readFile(filePath, 'utf8');
@@ -18,7 +19,6 @@ export async function initialSetup(): Promise<void> {
         const tokens = tokenize(content);
         const ast = parse(tokens);
         const compiledContent = await compile(ast);
-
         const injectedContent = await injectScripts(compiledContent);
 
         await globalThis.config.shopifyClient.uploadFile(`layout/${file}`, injectedContent);
@@ -29,6 +29,7 @@ export async function initialSetup(): Promise<void> {
 
   await Promise.allSettled(filePromises);
 
+  // eslint-disable-next-line no-useless-return
   return;
 }
 
