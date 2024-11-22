f9a25fd5a0fef9b84e0c27b13ea96763d57689f4
zacariec <10770942+zacariec@users.noreply.github.com>
2024-11-22 14:20:35 +1100

    Updated the watch command.
    - Deleted the main watch file.
    - Componetized watch
    - Moved watch into a folder with commander & watch.
    - Watch.tsx wraps our components and provides context to them.
    - Commander houses and exports our watch command to root.

--- ts-cli/src/commands/root.ts
+++ ts-cli/src/commands/root.ts
@@ -2,43 +2,43 @@ import { Command } from 'commander';
 
 import buildCommand from '@commands/build.js';
 import deployCommand from '@commands/deploy.js';
-import watchCommand from '@commands/watch.js';
+import watchCommand from '@commands/watch/commander.js';
 import { LogError, LogInfo } from '@utils/logger.js';
 import { version } from '../../package.json';
 
 const program = new Command();
 
 program
-	.name('framework')
-	.description(
-		`Framework is a developer first web framework for building Shopify Liquid Storefronts.
+  .name('framework')
+  .description(
+    `Framework is a developer first web framework for building Shopify Liquid Storefronts.
 Framework is known for providing a world-class developer experience for Shopify Storefronts,
 providing a "framework" layer over the stock Liquid experience. Framework was designed to be
 build to server first, fast by default, easy to use, developer-focused and a seamless integration for
 existing Shopify Liquid Storefronts.`,
-	)
-	.version(version)
-	.addCommand(buildCommand)
-	.addCommand(watchCommand)
-	.addCommand(deployCommand);
-
+  )
+  .version(version)
+  .addCommand(buildCommand)
+  .addCommand(watchCommand)
+  .addCommand(deployCommand);
 
 program
-	.option('c, --config <path>', 'Path to the Framework configuration file')
-	.hook('preAction', async (thisCommand) => {
-		const options = thisCommand.opts();
-		if (options.config) {
-			LogInfo(`Loading config from ${options.config}`);
-		}
-	})
+  .option('c, --config <path>', 'Path to the Framework configuration file')
+  .hook('preAction', async (thisCommand) => {
+    const options = thisCommand.opts();
+    if (options.config) {
+      LogInfo(`Loading config from ${options.config}`);
+    }
+  });
 
 const run = async (argv: string[] = process.argv): Promise<void> => {
-	try {
-		await program.parseAsync(argv);
-	} catch (error) {
-		LogError(`An error occurred: ${error as Error}`);
-		process.exit(1);
-	}
-}
+  try {
+    await program.parseAsync(argv);
+  } catch (error) {
+    LogError(`An error occurred: ${error as Error}`);
+    process.exit(1);
+  }
+};
 
 export { run };
+
---
+++ ts-cli/src/commands/watch/watch.tsx
@@ -0,0 +1,68 @@
+import React, { createContext, useContext, useState } from 'react';
+import { Box, render, useApp, useInput } from 'ink';
+
+import type { FrameworkEnvironmentConfig } from '../../types/types.js';
+
+import { loadFrameworkConfig, setupGlobalConfig } from '@config/config.js';
+import { EnvironmentSelector } from '@components/EnvironmentSelector.js';
+import { WatchLogger } from '@components/WatchLogger.js';
+
+type Props = {
+  args: any;
+  environments: Record<string, FrameworkEnvironmentConfig>;
+};
+
+type State = {
+  environment: FrameworkEnvironmentConfig;
+  key: string;
+};
+
+type WatchContextType = {
+  state: State;
+  setState: (arg0: State) => void;
+};
+
+export const WatchContext = createContext<WatchContextType>({
+  state: {
+    key: '',
+    environment: {
+      themeId: 0,
+      shopifyUrl: '',
+      accessToken: '',
+    }
+  },
+  setState: (state: State) => {},
+});
+
+export const Watch = ({ args, environments }: Props) => {
+  const context = useContext(WatchContext);
+  const [state, setState] = useState(context.state);
+  const app = useApp();
+
+  useInput((input, key) => {
+    if (key.escape || input === 'q') {
+      app.exit();
+      process.exit(0);
+    }
+  });
+
+  return (
+    <WatchContext.Provider value={{ state, setState }}>
+      <Box flexDirection="column" borderStyle="bold" padding={2} margin={2} borderColor="green">
+        {state.environment.accessToken === '' ||
+        (!args?.environment && state.environment.accessToken === '') ? (
+          <EnvironmentSelector environments={environments} />
+        ) : (
+          <WatchLogger {...args} />
+        )}
+      </Box>
+    </WatchContext.Provider>
+  );
+};
+
+export async function WatchCommand(args: any): Promise<void> {
+  const frameworkConfig = await loadFrameworkConfig();
+  setupGlobalConfig(args, frameworkConfig);
+
+  render(<Watch {...args} environments={frameworkConfig.framework.environments} />);
+}
