5fd095db939dd23a68258262f5ac2929dd8a7f7b
zacariec <10770942+zacariec@users.noreply.github.com>
2024-11-26 17:43:23 +1100

    Updated events - added support for websocket and typed events.
    - Created a typed event emitter which takes 2 generics.

--- ts-cli/src/constants/events.ts
+++ ts-cli/src/constants/events.ts
@@ -12,4 +12,41 @@ export enum WatchEventEmitterEvents {
   ViteWarning = 'viteWarning',
 }
 
-export const WatchEventEmitter = new EventEmitter();
+export const WatchEventEmitter = new EventEmitter().setMaxListeners(1);
+
+export enum WebsocketClientEvents {
+  Change = 'change',
+  Error = 'error',
+  State = 'status',
+}
+
+export type FrameworkEvent = {
+  type: WebsocketClientEvents,
+  data: {
+    message: string;
+    file: string;
+  };
+};
+
+export class TypedEventEmitter<Events extends string | symbol, Payload> {
+  private eventEmitter: EventEmitter;
+
+  constructor(emitter: EventEmitter) {
+    this.eventEmitter = emitter;
+  }
+
+  public emit(event: Events, payload: Payload) {
+    this.eventEmitter.emit(event, payload);
+  }
+
+  public off(event: Events, listener: (...args: any[]) => void) {
+    this.eventEmitter.off(event, listener);
+  }
+
+  public on(event: Events, listener: (...args: any[]) => void) {
+    this.eventEmitter.on(event, listener);
+  }
+}
+
+export const WebsocketEventEmitter = new TypedEventEmitter<WebsocketClientEvents, FrameworkEvent>(new EventEmitter().setMaxListeners(1));
+
